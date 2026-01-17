f5d8272714b43cc76b93cc4bf1f58564a4edb60f
shuaibkhandev <skhan.csit@gmail.com>
2026-01-17 13:43:51 +0500

    work with fs

--- script.js
+++ script.js
@@ -1,9 +1,24 @@
-const {add, sub} = require("./math");
-
-console.log("Hello World");
-
-
-console.log(add(5,7));
-console.log(sub(10,4));
+// const {add, sub} = require("./math");
+const fs = require("fs");
+// console.log("Hello World");
 
 
+// console.log(add(5,7));
+// console.log(sub(10,4));
+
+// with synchronous 
+// const res  = fs.writeFileSync("test.txt", "Hey There!");
+
+// with Asynchronous 
+// fs.writeFile("test.txt", "Hey There from Async", (err)=>{});
+
+// const res = fs.readFileSync("./contacts.txt", "utf-8");
+// console.log(res);
+
+fs.readFile("./contacts.txt", "utf-8", (err, res)=>{
+    if(err){
+        console.log(err);
+    }else{
+        console.log(res);    
+    }
+})
